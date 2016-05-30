1cc26713628f3ded9ea7b2699743e32794101eb2
Hassan Salman <hassansalman8@gmail.com>
2016-05-29 22:23:46 -0700

    Configured estimate box

--- js/contact_me.js
+++ js/contact_me.js
@@ -13,6 +13,7 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
+            var message = $("input#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
@@ -23,7 +24,8 @@ $(function() {
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone
+                    phone: phone,
+                    message: message
                 },
                 cache: false,
                 success: function() {
@@ -45,7 +47,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + name + ", it seems that my mail server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
